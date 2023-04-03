8eaa565fdbe9ec76b136c36833f833a183ade61e
LINUX <linux@localhost.localdomain>
2023-04-03 16:01:10 +0300

    changes

---
+++ views/modules/footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="https://tulusdev.xyz" target="_blank">Tulus Developers</a></strong>
+    All Rights Reserved
+</footer>
---
+++ views/modules/menu.php
@@ -0,0 +1,65 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu">
+            <li class="active">
+                <a href="">
+                    <i class="fa fa-home"></i>
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-user"></i>
+                    <span>User</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-th"></i>
+                    <span>Categories</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-product-hunt"></i>
+                    <span>Products</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-users"></i>
+                    <span>Clients</span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a href="">
+                    <i class="fa fa-list-ul"></i>
+                    <span>Sales</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Manage Sales</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Create Sales</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Sales Repost</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+</aside>
